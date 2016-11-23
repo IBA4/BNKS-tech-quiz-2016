933714d813a952c2d4d215ad4dca1b758160202e
1BB3 <sanjeev1bb3@protonmail.com>
2016-11-24 00:43:16 +0545

    deleted unnecessary files

--- engine.js
+++
@@ -1,9 +0,0 @@
-/*
-
-Done by 1BA4 and 1BB3
-this engine uses jquery probably bootstrapjs and velocityjs
-for the mockup of MCQ.
-
-*/
-$(document).ready(function(){
-});
--- getContent.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $question_number = $_POST['number'];
-  require "permissions.php";
-  $qwerty = new permissions();
-  if ($qwerty->stop_update) {include "content.php";}
-?>
